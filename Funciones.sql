bb3a6adbe4ee24e558978486b8b103172fcfd2d2
Miguel Rodríguez <migueledo17@gmail.com>
2022-12-31 21:17:47 -0500

    Segunda función agregada

--- Funciones.sql
+++ Funciones.sql
@@ -1,5 +1,6 @@
 USE ProyectoUsuarios;
 
+-- Función que trae el salario promedio por cargos
 CREATE FUNCTION MediaSalarial ()
 RETURNS TABLE
 AS
@@ -8,3 +9,19 @@ RETURN (
 )
 
 SELECT *FROM MediaSalarial();
+
+SELECT *FROM Trabajadores;
+SELECT *FROM Empresa;
+
+CREATE OR ALTER FUNCTION SalarioPromedioCategoria (
+	@CategoriaEmpresa VARCHAR(30)
+)
+RETURNS TABLE
+AS
+RETURN (
+		SELECT Empresa.CategoriaEmpresa, Trabajadores.Cargo, ROUND(AVG(Trabajadores.SueldoDevengado), 2) AS SalarioPromedio FROM Empresa 
+		INNER JOIN Trabajadores ON Empresa.IdEmpresa = Trabajadores.IdTrabajador AND CategoriaEmpresa LIKE CONCAT('%', @CategoriaEmpresa, '%') 
+		GROUP BY Empresa.CategoriaEmpresa, Trabajadores.Cargo
+)
+
+SELECT *FROM SalarioPromedioCategoria('Finan');
